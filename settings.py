a8b6eb42a15866c76e0cccd2b97358912fdaab9f
zedmanish <manishkumar.zed@gmail.com>
2019-05-26 12:31:37 +0530

    database setting,py

--- settings.py
+++ settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jtsboard_new',
+		'USER': 'jts',
+		'PASSWORD' : 'Jts5678?',
+		'HOST' : '34.85.64.241',
+		'PORT' : '3306',    
+		}
 }
+
 
 
 # Password validation
